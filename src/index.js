81c2a4391c7116f5b0b60acc5b7e5a746b0292df
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-08-19 16:40:57 +0300

    summary

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import ReactDOM from 'react-dom';
-
+import { StrictMode } from 'react';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <StrictMode>
+        <App/>
+    </StrictMode>, document.getElementById('root');
+)
