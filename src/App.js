42f2fac9c1655cafa3a076af16f02683e6b0753d
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-08-20 15:24:26 +0300

    fix: component App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
+import { useState } from "react";
+import React from "react";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false)
+  const showPararaphHandler = () =>{
+    setShowParagraph(true)
+    if(showParagraph === true){
+      setShowParagraph(false)
+    }
+  }
   return (
+    <React.Fragment>
     <div className="app">
       <h1>React под Капотом</h1>
+      {showParagraph &&<p>Новый текст</p>}
     </div>
+    <button onClick={showPararaphHandler}>Нажми меня</button>
+</React.Fragment>
   );
 }
 
